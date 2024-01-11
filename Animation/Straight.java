db9ed17b7244a6f91fd043ac8ead6142b50dbf3b
Anchytca <126275328+Anchytca@users.noreply.github.com>
2024-01-11 23:34:52 +0300

    Delete Animation directory

--- Animation/Straight.java
+++
@@ -1,9 +0,0 @@
-package animation.straight;
-
-import java.io.IOException;
-
-public class Straight {
-    public static void main(String[] args) throws IOException {
-        new MyFrame();
-    }
-}
